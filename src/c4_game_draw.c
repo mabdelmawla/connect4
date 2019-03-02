a1ddce5f5135a0e977c635873aab0374bb2f1b11
mabdelmawla <mohamed.abdelmawla@gmail.com>
2019-03-02 20:40:52 +0100

    board draw completed

--- src/c4_game_draw.c
+++ src/c4_game_draw.c
@@ -9,59 +9,65 @@
 #include <ma_types.h>
 #include <c4_err.h>
 
+#define C4_GAME_DRAW_UNIT			120
+#define C4_CIRCLE_R					(C4_GAME_DRAW_UNIT * 8 / 20)
+#define C4_GAME_DRAW_NUM_H_UNIT		8
+#define C4_GAME_DRAW_NUM_W_UNIT		9
+#define C4_GAME_DRAW_WINDOW_HIGHT 	(C4_GAME_DRAW_NUM_H_UNIT * C4_GAME_DRAW_UNIT)
+#define C4_GAME_DRAW_WINDOW_WIDTH	(C4_GAME_DRAW_NUM_W_UNIT * C4_GAME_DRAW_UNIT)
+
 static _t_str_c4_hal_graphics_window gstr_main_win_cfg = {
-		.height =800,
-		.width = 800,
+		.height =C4_GAME_DRAW_WINDOW_HIGHT,
+		.width = C4_GAME_DRAW_WINDOW_WIDTH,
 		.bg_color = {0,0,0,0},
 		.pt_window_start = {0,0},
 		.win_name = "Connect 4"
 };
 
-static _t_str_c4_hal_object objs[50];
+static _t_str_c4_hal_object objs[50] = {0};
+static _t_str_c4_hal_object *g_slot_array[7];
+static int g_num_objs = 0;
+
+static void initialize_objects(void){
+	int cnt = 0;
+	//draw the game frame
+	objs[cnt].type = C4_HAL_GRAPHICS_RECTANGLE;
+	objs[cnt].color.b = 1;
+	objs[cnt].uni_obj.rectangle.bottom_left.x = C4_GAME_DRAW_UNIT;
+	objs[cnt].uni_obj.rectangle.bottom_left.y = C4_GAME_DRAW_UNIT;
+	objs[cnt].uni_obj.rectangle.top_right.x = C4_GAME_DRAW_WINDOW_WIDTH - C4_GAME_DRAW_UNIT;
+	objs[cnt].uni_obj.rectangle.top_right.y = C4_GAME_DRAW_WINDOW_HIGHT - C4_GAME_DRAW_UNIT;
+	cnt++;
+
+	//draw coin pointer
+	objs[cnt].type = C4_HAL_GRAPHICS_CIRCLE;
+	objs[cnt].color.r = 1;
+	objs[cnt].uni_obj.circle.center.x = C4_GAME_DRAW_UNIT * 3 / 2;
+	objs[cnt].uni_obj.circle.center.y = C4_GAME_DRAW_WINDOW_HIGHT - C4_GAME_DRAW_UNIT / 2;
+	objs[cnt].uni_obj.circle.radius = C4_CIRCLE_R;
+	objs[cnt].uni_obj.circle.finess = 120;
+	cnt++;
+
+	//draw circles array
+	for(int i = 0;i < 7; i++){
+		g_slot_array[i] = &objs[cnt];
+		for(int j = 0; j < 6; j++){
+			g_slot_array[i][j].type = C4_HAL_GRAPHICS_CIRCLE;
+			g_slot_array[i][j].uni_obj.circle.center.x = C4_GAME_DRAW_UNIT * (3+ 2 * i) / 2;
+			g_slot_array[i][j].uni_obj.circle.center.y = C4_GAME_DRAW_UNIT * (3+ 2 * j)/ 2;
+			g_slot_array[i][j].uni_obj.circle.radius = C4_CIRCLE_R;
+			g_slot_array[i][j].uni_obj.circle.finess = 120;
+			cnt++;
+		}
+	}
+	g_num_objs = cnt;
+}
 
 _t_c4_err c4_game_draw_init(_t_c4_hal_graphics_key_callback cb, _t_c4_hal_graphics_special_key_callback sp_cb){
 	_t_c4_err ret = C4_ERR_NOT_OK;
-	objs[0].type = C4_HAL_GRAPHICS_LINE;
-	objs[0].color.alfa = 0;
-	objs[0].color.r = 255.0;
-	objs[0].color.g = 0;
-	objs[0].color.b = 0;
-	objs[0].uni_obj.line.start.x = 100;
-	objs[0].uni_obj.line.start.y = 200;
-	objs[0].uni_obj.line.end.x = 200;
-	objs[0].uni_obj.line.end.y = 200;
 
-	objs[0].type = C4_HAL_GRAPHICS_RECTANGLE;
-	objs[0].color.alfa = 0;
-	objs[0].color.r = 0;
-	objs[0].color.g = 255;
-	objs[0].color.b = 0;
-	objs[0].uni_obj.rectangle.bottom_left.x = 120;
-	objs[0].uni_obj.rectangle.bottom_left.y = 120;
-	objs[0].uni_obj.rectangle.top_right.x = 240;
-	objs[0].uni_obj.rectangle.top_right.y = 240;
-
-	objs[1].type = C4_HAL_GRAPHICS_LINE;
-	objs[1].color.alfa = 0;
-	objs[1].color.r = 255.0;
-	objs[1].color.g = 0;
-	objs[1].color.b = 0;
-	objs[1].uni_obj.line.start.x = 100;
-	objs[1].uni_obj.line.start.y = 200;
-	objs[1].uni_obj.line.end.x = 200;
-	objs[1].uni_obj.line.end.y = 200;
-
-	objs[2].type = C4_HAL_GRAPHICS_CIRCLE;
-	objs[2].color.alfa = 0;
-	objs[2].color.r = 0;
-	objs[2].color.g = 0;
-	objs[2].color.b = 255;
-	objs[2].uni_obj.circle.center.x = 300;
-	objs[2].uni_obj.circle.center.y = 300;
-	objs[2].uni_obj.circle.radius = 100;
-	objs[2].uni_obj.circle.finess = 180;
-	c4_hal_graphics_draw_objects(3,objs);
-
+	initialize_objects();
+	c4_hal_graphics_draw_objects(g_num_objs,objs);
 	c4_hal_graphics_init(&gstr_main_win_cfg, cb, sp_cb);
 	return ret;
 }
