80f94fef3b8774893d96baad06aa5bf8eecbb6cb
mabdelmawla <mohamed.abdelmawla@gmail.com>
2019-03-03 18:50:11 +0100

    horizontal check is working fine

--- src/c4_game_play.c
+++ src/c4_game_play.c
@@ -10,17 +10,17 @@
 #include <c4_game_draw.h>
 #include <c4_hal_graphics.h>
 
-static _t_ma_u8 g_curr_coin_location = 0;
-static _t_ma_u8 g_curr_player = 0;
-static _t_ma_u8 g_board[7][6] = { 0xff };
-static _t_ma_u8 g_slots[7] = { 0 };
-static _t_ma_u8 g_declare_win = 0;
+static _t_ma_u8 g_curr_coin_location;
+static _t_ma_u8 g_curr_player;
+static _t_ma_u8 g_board[7][6];
+static _t_ma_u8 g_slots[7];
+static _t_ma_u8 g_declare_win;
 
 static void check_win(void) {
 	_t_ma_u8 loc = g_curr_coin_location;
-	_t_ma_u8 player = 1 - g_curr_player;
+	_t_ma_u8 player = g_curr_player;
 	int cnt;
-	int h = g_slots[loc]-1;
+	int h = g_slots[loc];
 	int win = 1;
 	//vertical check;
 	cnt = h;
@@ -37,6 +37,34 @@ static void check_win(void) {
 		c4_set_winning_coin(player);
 		return;
 	}
+
+	//horizontal check
+	win = 1;
+	cnt = loc;
+	//right side check
+	while(cnt > 0){
+		if(g_board[--cnt][h] == player){
+			win++;
+		} else {
+			break;
+		}
+	}
+	cnt = loc;
+	//left side check
+	while(cnt < 6){
+		if(g_board[++cnt][h] == player){
+			win++;
+		} else {
+			break;
+		}
+	}
+
+	if (win == 4) { // declare win
+		g_declare_win = 1;
+		c4_set_winning_coin(player);
+		return;
+	}
+
 
 	//horizontal check
 #if 0
@@ -64,7 +92,7 @@ static void check_win(void) {
 	}
 #endif
 }
-static void clear_all(void) {
+static void clear_all(_t_ma_bool update_draw) {
 	//g_curr_coin_location = 0;
 	//g_curr_player = 0;
 	g_declare_win = 0;
@@ -73,25 +101,29 @@ static void clear_all(void) {
 		g_slots[i] = 0;
 		for (int j = 0; j < 6; j++) {
 			g_board[i][j] = 0xff;
-			c4_game_board_update(i, j, 0xff);
+			if (update_draw) {
+				c4_game_board_update(i, j, 0xff);
+			}
 		}
 	}
-	c4_set_winning_coin(0xff);
+	if (update_draw) {
+		c4_set_winning_coin(0xff);
+	}
 }
 
 static void keyCB(_t_ma_char key, _t_str_c4_hal_graphics_point pt) {
-	if (1 == g_declare_win) {
-		clear_all();
+	if ((1 == g_declare_win) || ('n' == key) || ('N' == key)) {
+		clear_all(1);
 	} else {
 		if (('\r' == key) && (g_slots[g_curr_coin_location] < 6)) {
 			c4_game_board_update(g_curr_coin_location,
 					g_slots[g_curr_coin_location], g_curr_player);
 			g_board[g_curr_coin_location][g_slots[g_curr_coin_location]] =
 					g_curr_player;
+			check_win();
 			g_curr_player = 1 - g_curr_player;
 			c4_game_draw_coin(g_curr_coin_location, g_curr_player);
 			g_slots[g_curr_coin_location]++;
-			check_win();
 		}
 	}
 }
@@ -114,6 +146,9 @@ static void SpkeyCB(_t_ma_int key, _t_str_c4_hal_graphics_point pt) {
 
 int c4_game_play_init(void) {
 	int ret = -1;
+	g_curr_coin_location = 0;
+	g_curr_player = 0;
+	clear_all(0);
 	_t_str_c4_game_draw_init inp =
 			{ .pf_kcb = keyCB, .pf_skcb = SpkeyCB, .player_color[0] = { 1, 0, 0,
 					0 }, .player_color[1] = { 1, 1, 0, 0 } };
